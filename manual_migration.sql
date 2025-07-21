5381921217b33c8f461ed7a3da653ca97a0952d5
ohbotno <ohbotno@users.noreply.github.com>
2025-07-21 23:37:11 +0100

    chore: remove obsolete files (RISK_ASSESSMENT_TODO.md, manual_migration.sql)

--- manual_migration.sql
+++
@@ -1,7 +0,0 @@
--- Manual SQL migration to add requires_risk_assessment field
--- Run this in your SQLite database if automatic migration fails
-
-ALTER TABLE booking_resource ADD COLUMN requires_risk_assessment BOOLEAN DEFAULT 0;
-
--- Update the field to nullable
-UPDATE booking_resource SET requires_risk_assessment = 0 WHERE requires_risk_assessment IS NULL;
